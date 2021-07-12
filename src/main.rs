ab07d654f16b615ee00f5e6c9e0d51d9a95a3b03
Dy11on <26803229+Dy11on@users.noreply.github.com>
2021-07-11 23:59:23 -0400

    Setting up diesel mysql

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::mysql::MysqlConnection;
+use dotenv::dotenv;
+use std::env;
+use self::models::*;
+
+pub mod schema;
+pub mod models;
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
+    MysqlConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
 #[get("/")]
 async fn hello() -> impl Responder{
     HttpResponse::Ok().body("Hello World!")
@@ -14,12 +37,36 @@ async fn manual_hello() -> impl Responder{
     HttpResponse::Ok().body("Hey There!")
 }
 
+#[get("/database")]
+async fn query_data() -> impl Responder{
+    
+    use self::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results{
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
+
+    HttpResponse::Ok().body("check cmd")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     HttpServer::new(||{
         App::new()
             .service(hello)
             .service(echo)
+            .service(query_data)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("0.0.0.0:8000")?
